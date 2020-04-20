70f0d9df2b36de8b415a4214ba52d61551a60760
[username] <[email]>
2020-04-20 14:00:05 +0800

    -

--- src/main/java/com/csii/webhook/service/impl/LoginServiceImpl.java
+++ src/main/java/com/csii/webhook/service/impl/LoginServiceImpl.java
@@ -11,20 +11,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Service
 public class LoginServiceImpl implements LoginService {
 
-        @Autowired
-        private FindUsersDao findUsers;
-        @RequestMapping("/login")
-        public String login(String login, String password, String url, String state, Model model,String token)  {
-            Users user = findUsers.findUsers();
-            //将 传入的login password 与数据库中的 作对比，匹配则登录成功
-            if (login.equals(user.getLogin()) && password.equals(user.getPassword()) ){
-                String newUrl = url + "&code=" + token + "&state=" + state;
-                return "redirect:" + newUrl;
-            }else{
-                model.addAttribute("url",url);
-                model.addAttribute("state",state);
-                return "login";
-            }
-
+    @Autowired
+    private FindUsersDao findUsers;
+    @RequestMapping("/login")
+    public boolean login(String login, String password)  {
+        Users user = findUsers.findUsers();
+        //将 传入的login password 与数据库中的 作对比，匹配则登录成功
+        if (login.equals(user.getLogin()) && password.equals(user.getPassword()) ){
+            return true;
+        }else{
+            return false;
         }
     }
+
+
+    //    地址拼接
+    public String urlSplicing(boolean t,String url, String state,String token, Model model)  {
+        if (t==true){
+            String newUrl = url + "&code=" + token + "&state=" + state;
+            return newUrl;
+        }else{
+            model.addAttribute("url",url);
+            model.addAttribute("state",state);
+            return "login";
+        }
+    }
+}
