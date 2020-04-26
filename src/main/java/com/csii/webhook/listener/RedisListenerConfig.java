e592374a8442bbad45b348d779d65e8fd65cfd8e
wangxintao-csii <3043536511@qq.com>
2020-04-26 16:23:50 +0800

    监听器启动监听redis服务

--- src/main/java/com/csii/webhook/listener/RedisListenerConfig.java
+++ src/main/java/com/csii/webhook/listener/RedisListenerConfig.java
@@ -1,4 +1,43 @@
 package com.csii.webhook.listener;
 
-public class RedisListenerConfig {
+import com.csii.webhook.filter.SessionFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebListener
+public class RedisListenerConfig implements ServletContextListener {
+    @Value("${spring.redis.host}")
+    private String url;
+
+    @Value("${spring.redis.port}")
+    private int  port;
+
+    @Value("${spring.redis.password}")
+    private String  password;
+
+
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        boolean reslut =false;
+        reslut =  SessionFilter.getRedisResult(url,port,password);
+//        Map<String,Object> map =new HashMap<>();
+//
+//        if (reslut==false){
+//
+//        }
+    }
+
+
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
 }
