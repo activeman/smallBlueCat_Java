1c2191c1eff23210a144d104c657ddfa4c82e6b0
liushuai <270505081@qq.com>
2020-04-26 14:56:51 +0800

    -

--- src/main/java/com/csii/webhook/service/CommunictionService.java
+++ src/main/java/com/csii/webhook/service/CommunictionService.java
@@ -1,11 +1,11 @@
 package com.csii.webhook.service;
 
 
-import com.alibaba.da.coin.ide.spi.meta.ResultType;
 import com.alibaba.da.coin.ide.spi.standard.ResultModel;
 import com.alibaba.da.coin.ide.spi.standard.TaskResult;
 
 public interface CommunictionService {
     void printObject(Object query);
-    ResultModel<TaskResult>  responseTaskResult(String msg, ResultType resultType);
+    ResultModel<TaskResult>  responseTaskResult(String msg);//result
+    ResultModel<TaskResult>  responseTaskResult(String msg,Long intentId,String ... parameterNames);//ask_inf
 }
